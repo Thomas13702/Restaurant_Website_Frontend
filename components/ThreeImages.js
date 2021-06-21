26ab5536a5b8f8e07e714602e2421f78f3be75fa
Thomas13702 <thomaspritchard137@btinternet.com>
2021-06-21 18:38:57 +0100

    Added Three Images Component

--- components/ThreeImages.js
+++ components/ThreeImages.js
@@ -1,3 +1,48 @@
+import Image from "next/image";
+import styles from "@/styles/ThreeImages.module.scss";
+
 export default function ThreeImages() {
-  return <div>3 Images</div>;
+  return (
+    <div className={styles.body}>
+      <div className={styles.card}>
+        <Image
+          className={styles.image}
+          src={"/images/food.jpg"}
+          width={500}
+          height={500}
+        />
+        <h2>First Class Dining</h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+          perspiciatis cum quidem aut pariatur quod.
+        </p>
+      </div>
+      <div className={styles.card}>
+        <Image
+          className={styles.immage}
+          src={"/images/drinks.jpg"}
+          width={500}
+          height={500}
+        />
+        <h2>World Class Drinks</h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+          perspiciatis cum quidem aut pariatur quod.
+        </p>
+      </div>
+      <div className={styles.card}>
+        <Image
+          className={styles.immage}
+          src={"/images/bar.jpg"}
+          width={500}
+          height={500}
+        />
+        <h2>Ecstatic Atmosphere</h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+          perspiciatis cum quidem aut pariatur quod.
+        </p>
+      </div>
+    </div>
+  );
 }
