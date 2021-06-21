0479aa5780cd703a9ad4a2c298194e2c417cd87e
Thomas13702 <thomaspritchard137@btinternet.com>
2021-06-21 18:04:31 +0100

    Added Singular Image Component

---
+++ components/SingleImage.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import styles from "@/styles/SingleImage.module.css";
+
+export default function SingleImage() {
+  return (
+    <div className={styles.body}>
+      <div className={styles.card}>
+        <Image src={"/images/rooftop-bar1.jpg"} height={500} width={700} />
+        <div className={styles.text}>
+          <h4>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+            blanditiis tempora doloribus quisquam eligendi corporis?
+          </h4>
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import styles from "../styles/Home.module.css";
 import Layout from "@/components/Layout";
 import InfoPanel from "@/components/InfoPanel";
+import SingleImage from "@/components/SingleImage";
 import ThreeImages from "@/components/ThreeImages";
 
 export default function Home() {
   return (
     <Layout>
       <InfoPanel />
+      <SingleImage />
       <ThreeImages />
     </Layout>
   );
